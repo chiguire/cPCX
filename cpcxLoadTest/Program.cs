31c2ac0278c589b740b93a767cdb61fa2721e2eb
Ciro Duran <ciro.duran@gmail.com>
2026-03-08 23:06:25 +0000

    Initial commit of the load testing program

---
+++ cpcxLoadTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using NBomber.CSharp;
+
+namespace cpcxLoadTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Run();
+    }
+    
+    private static void Run()
+    {
+        var scenario = Scenario.Create("hello_world_scenario", async context =>
+            {
+                
+                // you can define and execute any logic here,
+                // for example: send http request, SQL query etc
+                // NBomber will measure how much time it takes to execute your logic
+                await Task.Delay(500);
+
+                return Response.Ok();
+            })
+            .WithoutWarmUp()
+            .WithLoadSimulations(
+                Simulation.Inject(rate: 150, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(30)) // keep injecting with rate 150
+            );
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+    }
+}
