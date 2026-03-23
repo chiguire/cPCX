bc97a409fa85d3e275f065924e1dcb27f562f659
Ciro Duran <ciro.duran@gmail.com>
2026-03-23 22:24:41 +0000

    qol

--- deploy/redeploy_landing_page.sh
+++ deploy/redeploy_landing_page.sh
@@ -2,4 +2,5 @@
 
 source ./secrets && \
 ANSIBLE_PYTHON_INTERPRETER=auto_silent ansible-playbook -v -K -i inventory.ini playbooks/redeploy_landing_page.yml \
-    -e "github_repo=${GITHUB_REPO}"
+    -e "github_repo=${GITHUB_REPO}" \
+    -e "emf_allowed_cidrs=${EMF_ALLOWED_CIDRS}"
