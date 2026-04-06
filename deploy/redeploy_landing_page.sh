4ff202d27fce361e7aa700770b64dc9ea3a4a3ce
Ciro Durán <ciro.duran@gmail.com>
2026-04-06 11:05:20 +0100

    scrub secrets example

--- deploy/redeploy_landing_page.sh
+++ deploy/redeploy_landing_page.sh
@@ -3,4 +3,4 @@
 source ./secrets && \
 ANSIBLE_PYTHON_INTERPRETER=auto_silent ansible-playbook -v -K -i inventory.ini playbooks/redeploy_landing_page.yml \
     -e "github_repo=${GITHUB_REPO}" \
-    -e "emf_allowed_cidrs=${EMF_ALLOWED_CIDRS}"
+    -e "allowed_cidrs=${ALLOWED_CIDRS}"
